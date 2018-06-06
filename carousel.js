15ca070284cfb3c0f11d16c169d993a52844f3e8
Paola Carreño <pishipao@gmail.com>
2018-06-06 17:59:29 -0500

    help

---
+++ carousel.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('.carrusel').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: false,
+    //autoplaySpeed: 2500
+  })
+})
